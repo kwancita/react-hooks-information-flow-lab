cf89b5d83b640e22784e1673ad20788d8ab6e150
Wasanta <kwan_u_day@hotmail.com>
2021-11-28 13:59:15 -0500

    Revert "add"
    This reverts commit 3835f90b3e0220002730774d427fe84e93008be6.

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,8 @@
-// import React, { useState } from "react";
-// import ShoppingList from "./ShoppingList";
-// import itemData from "../data/items";
-// import Header from "./Header";
-
-// function App() {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-
-  
-//   return (
-//     <div className = {"App " + (isDarkMode ? "dark" : "light")}>
-//       <Header isDarkMode={isDarkMode} setIsDarkMode = {setIsDarkMode}/>
-//       <ShoppingList items={itemData} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
-import Header from './Header'
 
 function App() {
-  const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
@@ -33,8 +11,13 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header onDarkModeClick={handleDarkModeClick} mode={isDarkMode}/>
-      <ShoppingList items={items} />
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      <ShoppingList items={itemData} />
     </div>
   );
 }
